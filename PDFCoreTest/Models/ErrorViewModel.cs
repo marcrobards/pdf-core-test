8368e25e076ee7703219be576bc047f911448164
Marc Robards <marc@robards.org>
2018-02-13 10:07:55 -0500

    Add project files.

---
+++ PDFCoreTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PDFCoreTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
